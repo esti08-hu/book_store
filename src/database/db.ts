1ae4f276f7ae66a9618795690f65b894d4d7905c
esti08-hu <estioame@gmail.com>
2024-11-24 22:29:27 +0300

    configure database connection to use environment variables and enable SSL

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,13 +1,17 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
 import * as schema from './schema';
 
+dotenv.config();
+
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'admin',
-  database: 'book_store_db',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  ssl: true,
 });
 
 export const db = drizzle(pool, { schema });
